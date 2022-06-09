8ffca1a71786d7bd03b41a9a085c084a459645c2
Marcello-Cruz <marcellinhopastelzinho@gmail.com>
2022-06-08 21:46:58 -0300

    add exerc completo e corrigido

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome, sobrenome;
+            Console.Write("Digite seu nome.......: ");
+            nome = Console.ReadLine();
+
+            Console.Write("Digite seu sobrenome..: ");
+            sobrenome = Console.ReadLine();
+
+            Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+            Console.WriteLine($"Nome de catálogo: {sobrenome.ToUpper()}, {nome}");
+        }
+    }
+}
